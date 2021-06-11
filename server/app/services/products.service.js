0d5b66afb2253aacd000da7b61c496afc9c124b3
ghDevil <51207608+ghDevil@users.noreply.github.com>
2021-06-12 01:47:10 +0545

    hello

--- server/app/services/products.service.js
+++ server/app/services/products.service.js
@@ -26,8 +26,8 @@ exports.add = (param) => {
 	    var decodedImg = decodeBase64Image(param.image);
 	    var imageBuffer = decodedImg.data;
 		var fileName =  param.image_title;
-		var filePath = "http://52.200.24.125/cfw-admin-server/client/public/uploads/"+fileName;
-		var uploadPath = "http://52.200.24.125/cfw-admin-server/client/public/uploads/"+fileName;
+		var filePath = "../../../../client/public/uploads/"+fileName;
+		var uploadPath = "/uploads/"+fileName;
 	    var startup_image = imageBuffer;
 	   console.log(startup_image);
 	   // Use the mv() method to place the file somewhere on your server
